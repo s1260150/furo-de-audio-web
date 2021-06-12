8d62fe845d5b270ca2530623119bc1ab6b6fb897
ume <s1260150@u-aizu.ac.jp>
2021-06-12 17:10:36 +0900

    add app bar and bottom nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,149 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            primaryTextTheme: TextTheme(title: TextStyle(color: Colors.black))),
+        home: MyApp(),
+      ),
+    );
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
+  int _navIndex = 0;
+  String _label = '';
+  final _titles = ['Home', 'Library', 'Settings'];
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text('furo-de-audio'),
+        actions: [
+          SearchBar(),
+          IconButton(
+            icon: const Icon(Icons.person, color: Colors.black),
+            onPressed: () {},
+          ),
+        ],
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_music),
+            title: Text('Library'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Settings'),
+          ),
+        ],
+        onTap: (int index) {
+          setState(
+            () {
+              _navIndex = index;
+              _label = _titles[index];
+            },
+          );
+        },
+        currentIndex: _navIndex,
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Center(
+              child: Text(
+                _label,
+                style: TextStyle(fontSize: 24.0),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class SearchBar extends StatefulWidget {
+  const SearchBar({Key? key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _SearchBarState createState() => _SearchBarState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _SearchBarState extends State<SearchBar>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> _animation;
+  late AnimationController _animController;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  final _focusNode = FocusNode();
+  bool _isFocus = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _animController =
+        AnimationController(duration: Duration(milliseconds: 300), vsync: this);
+
+    final curvedAnimation =
+        CurvedAnimation(parent: _animController, curve: Curves.easeOutSine);
+
+    _animation = Tween<double>(begin: 0, end: 150).animate(curvedAnimation)
+      ..addListener(() {
+        setState(() {});
+      });
+
+    _focusNode.addListener(() {
+      setState(() {
+        _isFocus = _focusNode.hasFocus;
+        if (_isFocus) {
+          _animController.forward();
+        } else {
+          _animController.reverse();
+        }
+        debugPrint("isFocus: " + _isFocus.toString());
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return Container(
+        width: 200,
+        height: 50,
+        child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+          Visibility(
+            visible: !_isFocus,
+            child: Container(
+                width: 50,
+                child: IconButton(
+                  icon: const Icon(Icons.search, color: Colors.black),
+                  onPressed: () {
+                    _focusNode.requestFocus();
+                  },
+                )),
+          ),
+          Container(
+              width: _animation.value,
+              height: 50,
+              child: Padding(
+                  padding: const EdgeInsets.only(right: 10.0, bottom: 5.0),
+                  child: TextField(
+                    focusNode: _focusNode,
+                    style: TextStyle(color: Colors.black),
+                  ))),
+        ]));
   }
 }
